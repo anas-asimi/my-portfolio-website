cd23b374bba73a103ccdf6261ca453b5d931be54
anas0bito <96184837+anas0bito@users.noreply.github.com>
2022-05-15 22:45:25 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -35,12 +35,32 @@ function remove_active() {
     links[a].classList.remove("active-link");
   }
 }
+// 
+let slideIndex = 1;
+showSlides(slideIndex);
 
-open_button.addEventListener("click", function () {
-  open_menu();
-});
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-close_button.addEventListener("click", function () {
-  close_menu();
-});
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "flex";
+  dots[slideIndex-1].className += " active";
+}
